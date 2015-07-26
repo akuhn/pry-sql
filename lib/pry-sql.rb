8466c8f5b8858f2d7ece94b8ae84ff7f8dd56323
Adrian Kuhn <akuhn@iam.unibe.ch>
2015-07-26 01:22:16 -0700

    Basic commands and connector for sqlite.

--- lib/pry-sql.rb
+++ lib/pry-sql.rb
@@ -1,23 +1,61 @@
 require "pry-sql/version"
-require "sqlite3"
 
 module PrySQL
 
-  DB = SQLite3::Database.new(':memory:')
-
-  %w(select create insert update show).each do |command|
-    Pry::Commands.create_command(command) do
-
-      match /(#{command}\s.*)/i
-
-      description "execute #{command.upcase} query"
-
-      command_options listing: command, keep_retval: true
-
-      def process
-        DB.execute(captures[0])
-      end
-    end
+  def self.database
+    @database
   end
 
+  def self.connect(cmd, fname)
+    unless fname == ':memory:' || fname.ends_with?('.sqlite') || File.exist?(fname)
+      cmd.output.puts "Error: file '#{fname}' not found."
+      return
+    end
+    require "sqlite3"
+    @database = SQLite3::Database.new(fname)
+  end
+
+  def self.execute(cmd, query)
+    if @database.nil?
+      cmd.output.puts 'Error: Not connected. Use `connect filename` to open database.'
+      return
+    end
+    @database.execute(query)
+  end
+
+end
+
+Pry::Commands.create_command 'connect' do
+
+  group 'pry-sql'
+  description 'open SQLite database'
+  command_options argument_required: true, requires_gem: ['sqlite3']
+
+  def process(fname)
+    PrySQL::connect(self, fname)
+  end
+end
+
+Pry::Commands.create_command 'query' do
+
+  group 'pry-sql'
+  description 'Execute SQL query'
+  command_options argument_required: true, keep_retval: true, requires_gem: ['sqlite3']
+
+  def process
+    PrySQL::execute(self, arg_string)
+  end
+end
+
+%w(SELECT INSERT UPDATE CREATE).each do |command|
+  Pry::Commands.create_command(command) do
+
+    group 'pry-sql'
+    description "Execute inline #{command.downcase} command."
+    command_options keep_retval: true, requires_gem: ['sqlite3']
+
+    def process
+      PrySQL::execute(self, "#{command_name} #{arg_string}")
+    end
+  end
 end
